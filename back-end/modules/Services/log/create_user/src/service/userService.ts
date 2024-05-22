0bd463848fec218c4134d7544a0221fff7dfb1a3
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-05-22 00:53:54 -0300

    <fix>[userService]:  erro de nao retornar em tempo no teste

--- back-end/modules/Services/log/create_user/src/service/userService.ts
+++ back-end/modules/Services/log/create_user/src/service/userService.ts
@@ -3,9 +3,9 @@ import { User } from '../models/user';
 
 let userIdCounter = 1;
 
-export const createUser = async (name: string, password: string, birthDate: Date, login: string, email: string, cpf: string) => {
-       const hashedPassword = await bcrypt.hash(password, 10);
+export const createUser =  (name: string, password: string, birthDate: Date, login: string, email: string, cpf: string) => {
+       const hashedPassword =  bcrypt.hash(password, 10);
        const user = new User({id: userIdCounter++, name, password: hashedPassword, birthDate, login, email, cpf});
-       await user.save();
+        user.save();
     return user;     
 };

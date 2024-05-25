76b417d40e28eb79623f360069c9a31df92eb7d6
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-05-25 01:46:43 -0300

    <refactor>[create_user]: reposicionamento da validaçao

--- back-end/modules/Services/log/create_user/src/service/userService.ts
+++ back-end/modules/Services/log/create_user/src/service/userService.ts
@@ -3,21 +3,12 @@ import { User } from '../models/user';
 
 let userIdCounter = 1;
 
-export const createUser =  (name: string, password: string, birthDate: string, login: string, email: string, cpf: string) => {
+export const createUser =  (name: string, password: string, birthDate: Date, login: string, email: string, cpf: string) => {
 
-    const dateParts = birthDate.split("/");
     
-    if (dateParts.length !== 3 || dateParts[0].length !== 2 || dateParts[1].length !== 2 || dateParts[2].length !== 4) {
-        throw new Error(`Invalid birthDate format. Expected format is dd/mm/yyyy.`);
-    }
-
-    const dateObject = new Date(Number(dateParts[2]), Number(dateParts[1]) - 1, Number(dateParts[0]));
-    if (isNaN(dateObject.getTime())) {
-        throw new Error('Invalid birthDate. Could not parse into a valid date');
-    }
 
     const hashedPassword = bcrypt.hashSync(password, 10);
-    const user = new User({id: userIdCounter++, name, password: hashedPassword, birthDate: dateObject, login, email, cpf});
+    const user = new User({id: userIdCounter++, name, password: hashedPassword, birthDate, login, email, cpf});
         user.save();
     return user;     
 };

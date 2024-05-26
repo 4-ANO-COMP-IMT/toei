f9a882e7f23d0d6ba15a3b280bef059930e428ff
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-05-25 23:16:59 -0300

    <chore>[entrega01]: implementa o primeiro microsserviço
    Este commit representa a entrega 01.
    
    Foi implementado o microsserviço de criação de usuário no MongoDb

--- back-end/modules/Services/log/create_user/src/service/userService.ts
+++ back-end/modules/Services/log/create_user/src/service/userService.ts
@@ -4,9 +4,6 @@ import { User } from '../models/user';
 
 export const createUser =  (name: string, password: string, birthDate: Date, login: string, email: string, cpf: string) => {
 
-    
-    
-
     const hashedPassword = bcrypt.hashSync(password, 10);
     const user = new User({name, password: hashedPassword, birthDate, login, email, cpf});
         user.save();

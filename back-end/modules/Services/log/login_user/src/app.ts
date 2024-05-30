6051e33b5376713ee1d04284cb13f8c523500f9c
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-05-30 10:42:40 -0300

    <feat>[login_user]: conexoes implementadas

--- back-end/modules/Services/log/login_user/src/app.ts
+++ back-end/modules/Services/log/login_user/src/app.ts
@@ -1,2 +1,29 @@
-let hello: string = "Hi, Typescript"
-console.log(hello)
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+import authRoutes from './routes/authRoutes';
+
+const app = express();
+
+if (!config.mongoUri || !config.port) {
+  console.error('Required configuration is missing');
+  process.exit(1);
+}
+
+mongoose.connect(config.mongoUri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
+
+const PORT = config.port;
+app.listen(PORT, () => {
+  console.log(`Create User Microservice is running on port ${PORT}`);
+});
+
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Create Auth Microservice');
+});

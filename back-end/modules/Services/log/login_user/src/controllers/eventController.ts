e5260955da20358930f67d5776dd7e066cb36951
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-06-02 14:21:21 -0300

    <refactor>[login_user]: retirada de consoles e teste nao condizente

--- back-end/modules/Services/log/login_user/src/controllers/eventController.ts
+++ back-end/modules/Services/log/login_user/src/controllers/eventController.ts
@@ -6,7 +6,7 @@ type FuncoesKeys = keyof typeof funcoes;
 
 export const handleEvent = app.post('/event', async (req, res) => {
     try{
-        console.log("---------------------------------------------------------------/EVENT")
+
         const type: string = req.body.type
         const payload = req.body.payload
         if (type in funcoes) {
@@ -24,15 +24,11 @@ export const handleEvent = app.post('/event', async (req, res) => {
 
 const funcoes = {
     UserRegistered:(user: any)=>{
-        console.log("---------------------------------------------------------------/EVENT/USERREGISTERED")
+
         console.log(user)
         // pega o login e senha
         const {login,password} = user;
-        console.log("----------------------------------------------------------login")
-        console.log(login)
-        console.log("----------------------------------------------------------password")
-        console.log(password)
-        console.log("----------------------------------------------------------authLogin")
+
         // e registra na tabela login{login,senha}
         const authLogin = new UserLogin({login, password});
         console.log(authLogin)
--- back-end/modules/Services/log/login_user/src/service/authService.ts
+++ back-end/modules/Services/log/login_user/src/service/authService.ts
@@ -2,14 +2,11 @@ import bcrypt from 'bcrypt';
 import { UserLogin } from '../models/userLogin';
 
 export const login = async (login: string, password: string) => {
-  console.log('login', login);
-  console.log('password', password)
+ 
   const userLogin = await UserLogin.findOne({login: login});
-  console.log('userLogin', userLogin)
+
   if (!userLogin || !(await bcrypt.compare(password, userLogin?.password || ''))){
-    console.log("------------FALSE")
     return false;
   }
-  console.log("------------TRUE")
   return true;
 };

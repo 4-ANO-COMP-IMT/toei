5c7e6ef8fafd1f60d481a6b48fabd7798f0d8ebf
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-06-02 13:38:33 -0300

    <refactor>[Bridge]: otimização do barramento (login e user)

--- back-end/modules/Bridge/src/app.ts
+++ back-end/modules/Bridge/src/app.ts
@@ -14,42 +14,19 @@ app.use(express.json())
 app.post('/event', async (req, res)=>{
   const event = req.body
   console.log(event)
-try{
-    axios.post('http://localhost:3000/user/event', event)
-    .then(res => console.log(res.data)).catch(err => {
-        console.log(err);
-        err = err[3000] || []
-        err.push({port: 3000, event: event, err: err})
-        err[3000] = err
-    });
-    res.status(201).send({status: 'Event create user received'})
-}catch(err){
-    console.log('Failed to publich create user event',err)
-}
 
-// try{
-//     axios.post('http://localhost:4000/eventos', event)
-//     .then(res => console.log(res.data)).catch(err => {
-//         console.log(err);
-//         err = err[4000] || []
-//         err.push({port: 4000, evento: event, erro: err})
-//         err[4000] = err
-//     });
-//     res.status(201).send({status: 'Event login user received'})
-// }catch(err){
-//     console.log('Failed to publich login user event',err)
-// }
+  try{
+      axios.post('http://localhost:3000/user/event', event)
+  }catch(err){
+      console.log('Failed to send event to user',err)
+  }
 
+  try{
+    axios.post('http://localhost:4000/auth/event', event)
+  }catch(err){
+    console.log('Failed to send event to login',err)
+  }
 
-// res.end()
-// })
-  
-  // axios.post('http://localhost:4000/event', event)
-//   .catch((err) => {
-//     console.log('Failed to publich login user event',err)
-//   })
-//   res.status(201).send({status: 'Event login user received'})
-
-  res.end()
+  res.status(201).send({status: `Event "${event.type}" received`})
 })
 app.listen(PORT, () => console.log(`Event-Driven. Port: ${PORT}.`))

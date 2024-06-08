2543727615cbc0474188591f92875a3521568190
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-06-08 18:05:28 -0300

    <comment>[createUser]: código para teste do front

--- back-end/modules/Services/log/create_user/src/routes/userRoutes.ts
+++ back-end/modules/Services/log/create_user/src/routes/userRoutes.ts
@@ -5,6 +5,12 @@ import * as eventController from '../controllers/eventController';
 const router = Router();
 
 router.post('/', userController.register);
+
+// mss para teste do front
+// router.put('/teste', (req, res)=>{
+//     res.json({"mensagem":req.body.mensagem});
+// });
+
 router.get('/:login', userController.read);
 
 router.post('/event',eventController.handleEvent);

03ada604969a48f685063e2213a42a94e1db7379
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-06-28 20:41:41 -0300

    <feat>[front-end]: estrutura do front-end

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
 
-import CreateUserPage from './pages/createUserPage'
+import Register from './pages/register'
+import Login from './pages/login'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CreateUserPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Register/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        {/* Criar uma página pra erro? */}
+        <Route path='*' element={<div>404</div>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

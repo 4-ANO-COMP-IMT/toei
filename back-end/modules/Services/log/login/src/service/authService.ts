43b0c251a4791817538d280e44b58eb808727830
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-08-17 19:33:28 -0300

    <feat>[login]: implementado sistema de sessões

--- back-end/modules/Services/log/login/src/service/authService.ts
+++ back-end/modules/Services/log/login/src/service/authService.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import { UserLogin } from '../models/userLogin';
+import axios from 'axios';
 
 export const login = async (login: string, password: string) => {
  
@@ -8,5 +9,16 @@ export const login = async (login: string, password: string) => {
   if (!userLogin || !(await bcrypt.compare(password, userLogin?.password || ''))){
     return false;
   }
+
+  
+  axios.post('http://localhost:10000/event', {
+    type: 'UserLogged',
+    payload: {
+        login: userLogin.login
+    }
+  }).catch((err) => {
+      console.log('Failed to send UserLogged event',err)
+  });
+
   return true;
 };

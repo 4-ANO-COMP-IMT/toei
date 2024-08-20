74e374100e3aa4d237d3e551f909cfed123c02a7
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-08-20 10:19:18 -0300

    <refactor>[artwork]: melhora a validação dos parâmetros na request

--- back-end/modules/Services/work/artwork/src/controllers/artworkController.ts
+++ back-end/modules/Services/work/artwork/src/controllers/artworkController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as artworkService from '../service/artworkService';
-import { MissingParameters, WrongTypeParameters } from './errorsController';
+import { MissingParameters, WrongTypeParameters, Invalid } from './errorsController';
+import { Artwork } from '../models/artworks';
 
 declare module 'express-session' {
     interface SessionData {
@@ -11,34 +12,13 @@ declare module 'express-session' {
 export const create_artwork = async (req: Request, res: Response) => {
     try {
         if(req.session.login_cookie){
-            console.log('session set [cookies]: ',req.session.login_cookie)
-            const { artwork } = req.body;
             const login = req.session.login_cookie;
+            if (!login) {throw new MissingParameters('login');}
+            if (typeof login !== 'string') {throw new WrongTypeParameters('login');}
+
+            const artwork:Artwork = req.body.artwork;
+            isArtworkValid(artwork);
             
-            if (!login) {
-                throw new MissingParameters('login');
-                
-            }
-            if (!artwork) {
-                throw new MissingParameters('artwork');
-                
-            }else{
-                if(!artwork.title){
-                    throw new MissingParameters('title');
-                }
-                if(!artwork.description){
-                    throw new MissingParameters('description');
-                }
-                if(!artwork.counters){
-                    throw new MissingParameters('counters');
-                }
-                if(!artwork.informations){
-                    throw new MissingParameters('informations');
-                }
-                if(!artwork.img){
-                    throw new MissingParameters('image');
-                }
-            }
             console.log("Login:",login);
             console.log("Artwork:",artwork);
 
@@ -58,14 +38,18 @@ export const create_artwork = async (req: Request, res: Response) => {
 
 export const read_artwork = async (req: Request, res: Response) => {
     try{
-        console.log('session set [cookies]: ',req.session.login_cookie)
         if(req.session.login_cookie){
+
             const login = req.session.login_cookie;
-            
-            const position:number = Number(req.params.position);
+            if (!login) {throw new MissingParameters('login');}
+            if (typeof login !== 'string') {throw new WrongTypeParameters('login');}
+
+            const position = Number(req.params.position);
+            if(!position){throw new MissingParameters('position');}
+            if (isNaN(position) || !Number.isInteger(position)) {throw new WrongTypeParameters('position');}
+            if (position < 0) {throw new Invalid('position');}
             
             const artworkRead = await artworkService.readArtwork(login, position);
-
             res.status(200).json({ artworkRead });
         }else{
             res.status(401).json({ message: 'User not logged in' });
@@ -75,4 +59,52 @@ export const read_artwork = async (req: Request, res: Response) => {
         res.status(400).json({ message: (error as Error).message });
     }
 
+}
+
+ const isArtworkValid = (artwork: Artwork) => {
+    if (!artwork) {throw new MissingParameters('artwork');
+    }else{
+        if(!artwork.hasOwnProperty('title')){throw new MissingParameters('title');}
+        if(typeof artwork.title !== 'string'){throw new WrongTypeParameters('title');}
+        if(artwork.title.trim() === ""){throw new Invalid('title');}
+
+        if(!artwork.hasOwnProperty('description')){throw new MissingParameters('description');}
+        if(typeof artwork.description !== 'string'){throw new WrongTypeParameters('description');}
+
+        if(!artwork.hasOwnProperty('counters')){throw new MissingParameters('counters');}
+        if(typeof  artwork.counters !== 'object'){throw new WrongTypeParameters('counters');}
+        else{
+            artwork.counters.forEach(element => {
+                if(!element.hasOwnProperty('name')){throw new MissingParameters('counter_name');}
+                if(typeof element.name !== 'string'){throw new WrongTypeParameters('counter_name');}
+                if(element.name.trim() === ""){throw new Invalid('counter_name');}
+                if(!element.hasOwnProperty('value')){throw new MissingParameters('counter_value');}
+                if(isNaN(element.value) || !Number.isInteger(element.value)){throw new WrongTypeParameters('counter_value');}
+                if(element.value < 0 || element.value > element.maxValue){throw new Invalid('counter_value');}
+                if(!element.hasOwnProperty('maxValue')){throw new MissingParameters('counter_maxvalue');}
+                if(isNaN(element.maxValue) || !Number.isInteger(element.maxValue)){throw new WrongTypeParameters('counter_maxvalue');}
+                if(element.maxValue < 0){throw new Invalid('counter_maxvalue');}
+            });
+        }
+
+        if(!artwork.hasOwnProperty('tags')){throw new MissingParameters('tags');}
+        if(typeof artwork.tags !== 'object'){throw new WrongTypeParameters('tags');}
+        artwork.tags.forEach(element => {
+            if(typeof element !== 'string'){throw new WrongTypeParameters('tag');}
+            if(element.trim() === "" || element.length > 20){throw new Invalid('tag');}
+        });
+
+        if(!artwork.hasOwnProperty('informations')){throw new MissingParameters('informations');}
+        if(typeof artwork.informations !== 'object'){throw new WrongTypeParameters('informations');}
+        artwork.informations.forEach(element => {
+            if(!element.hasOwnProperty('name')){throw new MissingParameters('info_name');}
+            if(typeof element.name !== 'string'){throw new WrongTypeParameters('info_name');}
+            if(element.name.trim() === ""){throw new Invalid('info_name');}
+
+            if(!element.hasOwnProperty('content')){throw new MissingParameters('info_content');}
+            if(typeof element.content !== 'string'){throw new WrongTypeParameters('info_content');}
+            if(element.content.trim() === ""){throw new Invalid('info_content');}
+        });
+        if(!artwork.hasOwnProperty('img')){throw new MissingParameters('image');}
+    }
 }
--- back-end/modules/Services/work/artwork/src/models/artworks.ts
+++ back-end/modules/Services/work/artwork/src/models/artworks.ts
@@ -1,21 +1,21 @@
 import { Schema, model } from "mongoose";
 
-interface Counter {
+export interface Counter {
     name: string;
     value: number;      // default 0
-    maxvalue: number;   //de 0 até valor máximo, se = a 0, não tem limite
+    maxValue: number;   //de 0 até valor máximo, se = a 0, não tem limite
 }
 const CounterSchema = new Schema({
     name: { type: String, required: true },
-    value: { type: Number, required: false, default: 0 },
-    maxValue: { type: Number, required: false, default: 0 }
+    value: { type: Number, required: true, default: 0 },
+    maxValue: { type: Number, required: true, default: 0 }
   });
 
-interface Information {
+export interface Information {
     name: string;
     content: string;
 }
-
+// make sure name and content are required
 const InformationSchema = new Schema({
     name: { type: String, required: true },
     content: { type: String, required: true }
@@ -23,6 +23,7 @@ const InformationSchema = new Schema({
 
 export interface Artwork {
     title: string;
+    description: string;
     counters: Counter[];
     tags: string[];
     informations: Information[];
@@ -31,10 +32,11 @@ export interface Artwork {
 
 const ArtworkSchema = new Schema<Artwork>({
     title: { type: String, required: true },
-    counters: { type: [CounterSchema], required: false, default: [] },
-    tags: { type: [String], required: false, default: [] },
-    informations: { type: [InformationSchema], required: false, default: [] },
-    img: { type: String, required: false, default: "" }
+    description: { type: String, required: true },
+    counters: { type: [CounterSchema], required: true, default: [] },
+    tags: { type: [String], required: true, default: [] },
+    informations: { type: [InformationSchema], required: true, default: [] },
+    img: { type: String, required: true, default: "" }
 });
 
 export interface Artworks {
@@ -44,7 +46,7 @@ export interface Artworks {
 
 const ArtworksSchema = new Schema<Artworks>({
     login: { type: String, required: true , unique: true},
-    artworks: { type: [ArtworkSchema], required: false, default: [] }
+    artworks: { type: [ArtworkSchema], required: true, default: []}
 });
 
 export const ArtworksModel = model<Artworks>("User", ArtworksSchema);

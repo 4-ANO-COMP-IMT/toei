be78c795a251f308c7ad56caf2800a192a0ba2cf
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-08-23 00:02:36 -0300

    <feat>[Bridge]: Adiciona o middleware cors

--- back-end/modules/Bridge/src/app.ts
+++ back-end/modules/Bridge/src/app.ts
@@ -1,17 +1,27 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import axios from 'axios'
+import { Request, Response } from 'express'
 
 dotenv.config()
-const { PORT } = process.env
+const { PORT, ALLOWED_PORTS } = process.env;
+const allowedPorts = ALLOWED_PORTS ? ALLOWED_PORTS.split(',') : [];
+const allowedOrigins = allowedPorts.map(port => `http://localhost:${port}`);
 
-const app = express()
+const cors = require('cors');
+const app = express();
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
+app.use(express.json());
 
-app.use(express.json())
+// axios.defaults.withCredentials = true;
 
-app.post('/event', async (req, res)=>{
-  const event = req.body
-  console.log(event)
+app.post('/event', async (req:Request, res:Response)=>{
+  const event = req.body;
+  // console.log(event)
 
   try{
       axios.post('http://localhost:3000/user/event', event)
@@ -33,4 +43,5 @@ app.post('/event', async (req, res)=>{
 
   res.status(201).send({status: `Event "${event.type}" received`})
 })
+
 app.listen(PORT, () => console.log(`Event-Driven. Port: ${PORT}.`))

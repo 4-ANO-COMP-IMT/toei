34b1f789b211386d4f9af9d199fcc05886bd1f6c
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-08-23 02:21:28 -0300

    <feat>[user]: implementa configuração de cors

--- back-end/modules/Services/log/user/src/app.ts
+++ back-end/modules/Services/log/user/src/app.ts
@@ -1,18 +1,23 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes';
 import mongoose from 'mongoose';
 import { config } from './config/config';
 
 const cors = require('cors');
 const app = express();
-app.use(cors())
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  optionsSuccessStatus: 200
+}))
+
+app.use(express.json());
 
 if (!config.mongoUri || !config.port) {
   console.error('Required configuration is missing');
   process.exit(1);
 }
-
 mongoose.connect(config.mongoUri)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
@@ -21,7 +26,5 @@ const PORT = config.port;
 app.listen(PORT, () => {
   console.log(`Create User Microservice is running on port ${PORT}`);
 });
-
-app.use(bodyParser.json());
 
 app.use('/user', userRoutes);

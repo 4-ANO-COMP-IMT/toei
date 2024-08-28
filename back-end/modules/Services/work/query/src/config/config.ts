eed253f12e90398ca600a2f728945b9adeb1dcdc
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-08-28 06:24:40 -0300

    <refactor>[query]: remove configurações não utilizadas

--- back-end/modules/Services/work/query/src/config/config.ts
+++ back-end/modules/Services/work/query/src/config/config.ts
@@ -5,16 +5,5 @@ dotenv.config();
 export const config ={
   port: process.env.PORT, 
   mongoUri: process.env.MONGOURI_CONNECTION,
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  redisPassword: process.env.REDIS_PASSWORD,
   sessionSecret: process.env.SESSION_SECRET,
-  sessionMaxAge:(
-    (
-      (
-        Number(process.env.SESSION_MAX_AGE_D) * 24 +
-        Number(process.env.SESSION_MAX_AGE_H)
-      ) * 60
-    ) + Number(process.env.SESSION_MAX_AGE_M)
-  ) * 60 * 1000
 };

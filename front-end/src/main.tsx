63db7d790500e12df0a2b3702d0b395c2d926656
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-09-01 16:42:46 -0300

    <feat>[front-end]: implementa as Routes para todas as pages

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -8,6 +8,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from './pages/register'
 import Login from './pages/login'
 import Home from './pages/home'
+import Artwork from './pages/artwork'
+import Edit from './pages/updateArtwork'
+import Create from './pages/create'
+import Profile from './pages/updateUser'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -17,6 +21,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/artwork/:artworkId' element={<Artwork/>}/>
+        <Route path='/edit/:artworkId' element={<Edit/>}/>
+        <Route path='/create/' element={<Create/>}/>
+        <Route path='/profile' element={<Profile/>}/>
         {/* Criar uma página pra erro? */}
         <Route path='*' element={<div>404</div>}/>
       </Routes>

2c566c69d80c2748df4fe601fd8e3b831053d363
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-10-20 13:12:14 -0300

    <feat>[lib]: implementa rotas

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/login.dart';
+import 'package:frontend/screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -8,6 +9,10 @@ void main() {
       primarySwatch: Colors.blue,
     ),
     home: LoginPage(),
-    debugShowCheckedModeBanner: false, // Remove a faixa de debug
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/login': (context) => LoginPage(),
+      '/home': (context) => HomePage(),
+    },
   ));
 }

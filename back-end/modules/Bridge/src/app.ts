33041bca9c0de0a4aa7d6464c812743cb8f79d8c
Gabriel-HBR <bacarado.gabriel@gmail.com>
2024-10-20 06:10:28 -0300

    <refactor>[Bridge]: remove variáveis

--- back-end/modules/Bridge/src/app.ts
+++ back-end/modules/Bridge/src/app.ts
@@ -4,17 +4,15 @@ import axios from 'axios'
 import { Request, Response } from 'express'
 
 dotenv.config()
-const { PORT, ALLOWED_URLS, ALLOWED_PORTS, ALLOWED_DIRS, PORT_NAMES } = process.env;
+const { PORT, ALLOWED_URLS, ALLOWED_DIRS, PORT_NAMES } = process.env;
 const allowedUrls = ALLOWED_URLS ? ALLOWED_URLS.split(','): [];
-const allowedPorts = ALLOWED_PORTS ? ALLOWED_PORTS.split(',') : [];
 const allowedDir = ALLOWED_DIRS ? ALLOWED_DIRS.split(',') : [];
-const allowedOrigins = allowedPorts.map((port,i) => `http://${allowedUrls[i]}:${port}`);
 const portNames = PORT_NAMES ? PORT_NAMES.split(',') : [];
 
 const cors = require('cors');
 const app = express();
 app.use(cors({
-  origin: allowedOrigins,
+  origin: allowedUrls,
   credentials: true,
   optionsSuccessStatus: 200
 }));
@@ -23,15 +21,15 @@ app.use(express.json());
 app.post('/event', async (req:Request, res:Response)=>{
   const event = req.body;
   console.log(event.type, 'event received');
-  
-  for (let i = 0; i < allowedPorts.length; i++) {
+
+  allowedUrls.forEach((url,i) => {
     try {
-      axios.post(`${allowedUrls[i]}:${allowedPorts[i]}/${allowedDir[i]}/event`, event)
+      axios.post(`${url}/${allowedDir[i]}/event`, event)
     } catch (err) {
       console.log(`Failed to send event to ${portNames[i]}`, err)
     }
-  }
-  res.status(201).send({status: `Event "${event.type}" received`})
+  });
+  res.status(201).send({status: `Event "${event.type}" received`});
 })
 
 app.listen(PORT, () => console.log(`Event-Driven. Port: ${PORT}.`))
